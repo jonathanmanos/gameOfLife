7ea984a0b5bb95d445f73a26fefaf1fc22351452
jonathanmanos <jonathanmanos@gmail.com>
2016-02-09 00:07:08 -0500

    first working version

--- gameOfLife.cc
+++ gameOfLife.cc
@@ -1,59 +1,227 @@
 /*
- * gameOfLife.cc - Implementation of person in the game of life
- * CPS222 Project 1
- *
- * Copyright (c) 2016 - Jonathan Manos
- *
- */
+* gameOfLife.cc - Implementation of the game of Life
+* CPS222 Project 1
+*
+* Copyright (c) 2016 - Jonathan Manos, Alonzo Ouzts
+*
+*/
 
 #include <iostream>
 #include <string>
 #include <sstream>
-#include <cmath>
-#include "person.h"
 
 using std::cerr;
 using std::cin;
 
+//Global variables
+std::string organismInput = "";
+std::string locationInputRow = "";
+std::string locationInputCol = "";
+std::string generationsInput = "";
+std::string dashes = "+ ";
+std::string organismLine = "";
+int locationRow = 0;
+int locationCol = 0;
+int amountGenerations = 4;
+int amountOrganisms = 0;
+int surroundingCounter = 0;
+
+
+
+//Constants
+static const int activeRows = 18;
+static const int activeCols = 50;
+static const int totalRows = activeRows + 2;
+static const int totalCols = activeCols + 2;
+static const char ESC = 27;
+
+
 //test value of deleting same node twice
 int main(int argc, char * argv [])
 {
-  std::string input = "";
 
-  while(input != "0")
+  enum Organism {NONE, GESTATING, LIVING, DYING};
+
+  Organism board[totalRows][totalCols];
+
+  Organism currentOrganism;
+
+  for (int i = 0; i < totalRows; i++)
   {
+    for (int j = 0; j < totalCols; j++)
+    {
+      board[i][j] = NONE;
+    }
+  }
+
+  std::stringstream st;
+
+  // Set amount of organisms
+  std::cerr << "\nHow many organisms initially? ";
+  getline(cin, organismInput);
+  st.str(organismInput);
+  st >> amountOrganisms;
+  st.clear();
+
+
+
+  for(int i = 0; i < amountOrganisms; i++)
+  {
+    // Set locations of organisms
+    std::cerr << "\nLocation of organism " << i+1 << " is on row? ";
+    getline(cin, locationInputRow);
+    st.str(locationInputRow);
+    st >> locationRow;
+    st.clear();
+
+
+    std::cerr << "\nLocation of organism " << i+1 << " is on column? ";
+    getline(cin, locationInputCol);
+    st.str(locationInputCol);
+    st >> locationCol;
+    st.clear();
+
+    board[locationRow][locationCol] = LIVING;
+  }
 
 
   // Set value to the string for new person
-  std::cerr << "Please enter a state for the first person" <<
-    " in your village (enter 0 to exit): \n" << std::endl;
-  getline(cin, input);
+  std::cerr << "\nGenerations? ";
+  getline(cin, generationsInput);
+  st.str(generationsInput);
+  st >> amountGenerations;
+  st.clear();
 
-	// Creates and initializes a new person with pointer.
-      	Person * p = new Person();
-
-        p -> setState(input);
-
-	// Creates and initializes a new node with pointer.
-  //    	Node * q = p;
+  std::cerr << "\n";
 
 
-	// Delete the node pointer.
-	//delete p;
-	//delete q;
-bool nonZero = (input!="0");
-bool validState = (input=="happy" || input=="sad");
 
-	// Write out the person's state.
-    if(validState && nonZero)
+  for (int i = 0; i < totalCols; i++)
+  {
+    dashes.append("- ");
+    if(i == totalCols-1)
+    dashes.append("+");
+  }
+
+
+  for (int g = 0; g < amountGenerations + 1; g++)
+  {
+
+    if(g==0)
     {
-		    std::cerr << "\nYour first villager is "
-		      <<  p -> getState()  << ".\n" << std::endl;
+      std::cout << "Initial:" << std::endl;
     }
-    else if(!validState && nonZero)
+
+    //if person is GESTATING at start of generation they are now LIVING
+    //if person is DYING at start of generation they are now NONE
+    if(g!=0)
     {
-      std::cerr << "\n" << p -> getState() << " is not a valid state"
-        << ".\n" << std::endl;
+      for (int i = 0; i < totalRows; i++)
+      {
+        for (int j = 0; j < totalCols; j++)
+        {
+          if(board[i][j] == GESTATING)
+          {
+            board[i][j] = LIVING;
+          }
+          if(board[i][j] == DYING)
+          {
+            board[i][j] = NONE;
+          }
+        }
+      }
     }
+
+
+    if(g!=0)
+    {
+      std::cout << "Generation "
+      << g << ":" << std::endl;
+    }
+
+    for (int i = 0; i < totalRows; i++)
+    {
+      organismLine.append("|");
+
+      if(i==0)
+      {
+        std::cerr << dashes << std::endl;
+      }
+
+      for (int j = 0; j < totalCols; j++)
+      {
+        if(g != 0)
+        {
+
+          if(board[i-1][j-1] == LIVING || board[i-1][j-1] == DYING)
+            surroundingCounter+=1;
+          if(board[i-1][j] == LIVING || board[i-1][j] == DYING)
+            surroundingCounter+=1;
+          if(board[i-1][j+1] == LIVING || board[i-1][j+1] == DYING)
+            surroundingCounter+=1;
+          if(board[i][j-1] == LIVING || board[i][j-1] == DYING)
+            surroundingCounter+=1;
+          if(board[i][j+1] == LIVING || board[i][j+1] == DYING)
+            surroundingCounter+=1;
+          if(board[i+1][j-1] == LIVING || board[i+1][j-1] == DYING)
+            surroundingCounter+=1;
+          if(board[i+1][j] == LIVING || board[i+1][j] == DYING)
+            surroundingCounter+=1;
+          if(board[i+1][j+1] == LIVING || board[i+1][j+1] == DYING)
+            surroundingCounter+=1;
+
+            //std::cout << surroundingCounter;
+
+          if((surroundingCounter < 2 || surroundingCounter > 3) && board[i][j] == LIVING)
+          {
+            board[i][j] = DYING;
+
+          }
+          if(surroundingCounter == 3 && board[i][j] == NONE)
+          {
+            board[i][j] = GESTATING;
+          }
+          surroundingCounter = 0;
+
+        }
+
+        if(board[i][j] == NONE)
+        {
+          organismLine.append("  ");
+          //std::cout << "NONE";
+        }
+        else if(board[i][j] == LIVING)
+        {
+          organismLine.append("L ");
+          //std::cout << "LIVING";
+        }
+        else if(board[i][j] == GESTATING)
+        {
+          organismLine.append("G ");
+        }
+        else if(board[i][j] == DYING)
+        {
+          organismLine.append("D ");
+        }
+      }
+
+
+      organismLine.append(" |");
+      std::cerr << organismLine << std::endl;
+      organismLine = "";
+
+      if(i == totalRows-1)
+      {
+        std::cerr << dashes << std::endl;
+      }
+
+    }
+
+    if(g != amountGenerations)
+    {
+      std::cout << "Press RETURN to continue";
+      while (cin.get() != '\n') ;
+    }
+
   }
 }
